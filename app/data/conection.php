5c920526e9abe6aad96eb950a8119af54f9b3b68
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2021-03-23 05:32:09 -0600

    Listado de ingresos y alta de nuevos ingresos. Listado de egresos

---
+++ app/data/conection.php
@@ -0,0 +1,7 @@
+<?php
+$conection = mysqli_connect("localhost","root","101217","estacionamientos");
+// if (!$conexion) {
+//     echo 'Error al conectar a la base de datos';
+// } else {
+//     echo 'Conectado a la base de datos';
+// }
